c40b52e51f24a1e595d2b8018d9f5f92ff40f617
jpggg <jcbergius@outlook.com>
2023-11-15 10:22:36 +0100

    Stable version refactor

--- Project/src/taskcontrol.adb
+++ Project/src/taskcontrol.adb
@@ -16,9 +16,9 @@ package body TaskControl is
         package front_sensor is new Ultrasonic(MB_P2,  MB_P0);
         package left_sensor  is new Ultrasonic(MB_P15, MB_P13);
         package right_sensor is new Ultrasonic(MB_P1,  MB_P16);
-        min_d : Distance_cm;
+        
         timer : Time;
-        PERIOD : Time_Span := Milliseconds(80);
+        PERIOD : constant Time_Span := Milliseconds(80);
         dt : Time_Span;
     begin
         loop
@@ -26,52 +26,8 @@ package body TaskControl is
             distance.front := front_sensor.Read;
             distance.left  := left_sensor.Read;
             distance.right := right_sensor.Read;
-            
-            if distance.front = 0 or distance.front > 40
-            then
-                distance.front := 40;
-            end if;
-            if distance.left = 0 or distance.left > 40
-            then
-                distance.left := 40;
-            end if;
-            if distance.right = 0 or distance.right > 40
-            then
-                distance.right := 40;
-            end if;
-           
-            
-            min_d := 41;
-            if (distance.right < min_d)
-            then
-                min_d := distance.right;
-            end if;
-            if (distance.front < min_d)
-            then
-                min_d := distance.front;
-            end if;
-            if (distance.left < min_d)
-            then
-                min_d := distance.left;
-            end if;
-              
-            
-            if (distance.right <= 20 and distance.right = min_d)
-            then
-                state := AvoidingRight;
-            elsif (distance.front <= 20 and distance.front = min_d)
-            then
-                state := AvoidingFront;
-            elsif (distance.left <= 20 and distance.left = min_d)
-            then
-                state := AvoidingLeft;
-            else
-                state := Remote;
-            end if;
-            Put_Line("state=" & state'Image);
-            
             dt := Clock - timer;
-            --Put_Line("RDS:");
+            --Put_Line("Read_Distance_Sensors:");
             --Put_Line("PERIOD= " & To_Duration(PERIOD)'Image);
             --Put_Line("dt= " & To_Duration(dt)'Image);
             --Put_Line("diff= " & To_Duration(PERIOD - dt)'Image);
@@ -81,12 +37,9 @@ package body TaskControl is
     
     
     task body Read_Radio is
-        RXdata : RadioData;
-        Threshold : constant := 20;
         timer : Time;
-        PERIOD : Time_Span := Milliseconds(20);
+        PERIOD : constant Time_Span := Milliseconds(20);
         dt : Time_Span;
-        
     begin
         Radio.Setup(RadioFrequency => 2511,
                     Length => 3+5,
@@ -98,59 +51,9 @@ package body TaskControl is
             timer := Clock;
             while Radio.DataReady loop
                 RXdata := Radio.Receive;
-                
-                -- Button states
-                if RXdata.Payload(5) = 2#001# then
-                    button.a := True;
-                    Put_Line("A");
-                else
-                    button.a := False;
-                end if;
-                if RXdata.Payload(5) = 2#010# then
-                    button.b := True;
-                    Put_Line("B");
-                else
-                    button.b := False;
-                end if;
-                if RXdata.Payload(5) = 2#100# then
-                    button.touch := True; 
-                    Put_Line("Touch");
-                else
-                    button.touch := False;
-                end if;
-                
-                -- Buttons determine rotation
-                if button.a and not button.b then
-                    acc.Rot := -2**9;
-                    Put_Line("A not B");
-                elsif not button.a and button.b then
-                    acc.Rot := 2**9 - 1;
-                    Put_Line("B not A");
-                else
-                    acc.Rot := 0;
-                end if;
-                
-                -- Accelerometer states
-                acc.X := LSM303AGR.Convert(RXdata.Payload(1), RXdata.Payload(2)) * Axis_Data(-1);
-                acc.Y := LSM303AGR.Convert(RXdata.Payload(3), RXdata.Payload(4));
-                
-                if abs(acc.X) < Threshold then
-                    acc.X := 0;
-                end if;
-                if abs(acc.Y) < Threshold then
-                    acc.Y := 0;
-                end if;
-                
-                -- Safe state when touch button is pressed
-                if button.touch then
-                    acc.X := 0;
-                    acc.y := 0;
-                    acc.Rot := 0;
-                end if;
-                
             end loop;
             dt := Clock - timer;
-            --Put_Line("Radio:");
+            --Put_Line("Read_Radio:");
             --Put_Line("PERIOD= " & To_Duration(PERIOD)'Image);
             --Put_Line("dt= " & To_Duration(dt)'Image);
             --Put_Line("diff= " & To_Duration(PERIOD - dt)'Image);
@@ -160,53 +63,141 @@ package body TaskControl is
     
     
     -- Think
-    task body Avoid_Front is
-        PERIOD : Time_Span := Milliseconds(80);
+    task body Determine_State is
+        minimum_distance : Distance_cm;
+        
+        timer : Time;
+        PERIOD : constant Time_Span := Milliseconds(80);
+        dt : Time_Span;
+    begin
+        loop
+            timer := Clock;
+            
+            -- Clamps distance values to range 1 .. 40 because we are not interested in > 40 or 0
+            if distance.front = 0 or distance.front > 40 then
+                distance.front := 40;
+            end if;
+            if distance.left = 0 or distance.left > 40 then
+                distance.left := 40;
+            end if;
+            if distance.right = 0 or distance.right > 40 then
+                distance.right := 40;
+            end if;
+           
+            -- Finds the lowest distance value
+            minimum_distance := 41;
+            if distance.right < minimum_distance then
+                minimum_distance := distance.right;
+            end if;
+            if distance.front < minimum_distance then
+                minimum_distance := distance.front;
+            end if;
+            if distance.left < minimum_distance then
+                minimum_distance := distance.left;
+            end if;
+              
+            -- Based on lowest distance value, determine which direction to Avoid
+            if distance.right <= 20 and distance.right = minimum_distance then
+                state := AvoidingRight;
+            elsif distance.front <= 20 and distance.front = minimum_distance then
+                state := AvoidingFront;
+            elsif distance.left <= 20 and distance.left = minimum_distance then
+                state := AvoidingLeft;
+            else
+                state := Remote;
+            end if;
+            
+            Put_Line("state=" & state'Image);
+            dt := Clock - timer;
+            delay until Clock + PERIOD;
+        end loop;
+    end Determine_State;
+
+    
+    task body Determine_Radio is
+        THRESHOLD : constant := 20;
+        
+        PERIOD : constant Time_Span := Milliseconds(20);
         timer : Time;
         dt : Time_Span;
     begin
         loop
             timer := Clock;
-            if state = AvoidingFront
-            then
+            
+            -- Button states
+            if RXdata.Payload(5) = 2#001# then
+                button.a := True;
+                Put_Line("A");
+            else
+                button.a := False;
+            end if;
+            if RXdata.Payload(5) = 2#010# then
+                button.b := True;
+                Put_Line("B");
+            else
+                button.b := False;
+            end if;
+            if RXdata.Payload(5) = 2#100# then
+                button.touch := True; 
+                Put_Line("Touch");
+            else
+                button.touch := False;
+            end if;
+            
+            -- Buttons determine rotation
+            if button.a and not button.b then
+                acc.Rot := -2**9;
+                Put_Line("A not B");
+            elsif not button.a and button.b then
+                acc.Rot := 2**9 - 1;
+                Put_Line("B not A");
+            else
+                acc.Rot := 0;
+            end if;
+            
+            -- Accelerometer states
+            acc.X := LSM303AGR.Convert(RXdata.Payload(1), RXdata.Payload(2)) * Axis_Data(-1);
+            acc.Y := LSM303AGR.Convert(RXdata.Payload(3), RXdata.Payload(4));
+            
+            if abs(acc.X) < Threshold then
+                acc.X := 0;
+            end if;
+            if abs(acc.Y) < Threshold then
+                acc.Y := 0;
+            end if;
+            
+            -- Safe state when touch button is pressed
+            if button.touch then
+                acc.X := 0;
+                acc.y := 0;
+                acc.Rot := 0;
+            end if;
+            
+            dt := Clock - timer;
+            delay until Clock + PERIOD;
+        end loop;
+    end Determine_Radio;
+    
+    
+    -- Act
+    task body Avoid is
+        PERIOD : constant Time_Span := Milliseconds(80);
+        timer : Time;
+        dt : Time_Span;
+    begin
+        loop
+            timer := Clock;
+            if state = AvoidingFront then
                 pwm.rf := -4095;
                 pwm.rb := -4095;
                 pwm.lf := -4095;
                 pwm.lb := -4095;
-            end if;
-            dt := Clock - timer;
-            delay until Clock + PERIOD;
-        end loop;
-    end Avoid_Front;
-    
-    task body Avoid_Left is
-        PERIOD : Time_Span := Milliseconds(80);
-        timer : Time;
-        dt : Time_Span;
-    begin
-        loop
-            timer := Clock;
-            if state = AvoidingLeft
-            then
+            elsif state = AvoidingLeft then
                 pwm.rf := -4095;
                 pwm.rb := 4095;
                 pwm.lf := 4095;
                 pwm.lb := -4095;
-            end if;
-            dt := Clock - timer;
-            delay until Clock + PERIOD;
-        end loop;
-    end Avoid_Left;
-    
-    task body Avoid_Right is
-        PERIOD : Time_Span := Milliseconds(80);
-        timer : Time;
-        dt : Time_Span;
-    begin
-        loop
-            timer := Clock;
-            if state = AvoidingRight
-            then
+            elsif state = AvoidingRight then
                 pwm.rf := 4095;
                 pwm.rb := -4095;
                 pwm.lf := -4095;
@@ -215,66 +206,42 @@ package body TaskControl is
             dt := Clock - timer;
             delay until Clock + PERIOD;
         end loop;
-    end Avoid_Right;
+    end Avoid;
     
-    task body Stopping is
-        PERIOD : Time_Span := Milliseconds(80);
-        timer : Time;
-        dt : Time_Span;
-    begin
-        loop
-            timer := Clock;
-            if state = Idle
-            then
-                pwm.rf := 0;
-                pwm.rb := 0;
-                pwm.lf := 0;
-                pwm.lb := 0;
-            end if;
-            dt := Clock - timer;
-            delay until Clock + PERIOD;
-        end loop;
-        
-    end Stopping;
     
-    task body Move_Remote is
+    task body Move_Radio is
         denominator : Integer;
-        factor : constant := 8;
+        FACTOR : constant := 8;
+        
         timer : Time;
-        PERIOD : Time_Span := Milliseconds(20);
+        PERIOD : constant Time_Span := Milliseconds(20);
         dt : Time_Span;
     begin
         loop
             timer := Clock;
             if state = Remote then
-                denominator := abs(Integer(acc.Y)*factor) + abs(Integer(acc.X)*factor) + abs(Integer(acc.Rot)*factor);
-                if (denominator < 1)
-                then
+                denominator := abs(Integer(acc.Y)*Factor) + abs(Integer(acc.X)*Factor) + abs(Integer(acc.Rot)*Factor);
+                if denominator < 1 then
                     denominator := 1;
                 end if;
                 
-                pwm.rf := Clamp(-4095, 4095, Integer(acc.Y*factor + acc.X*factor - acc.Rot*factor/2));
-                pwm.rb := Clamp(-4095, 4095, Integer(acc.Y*factor - acc.X*factor - acc.Rot*factor/2));
-                pwm.lf := Clamp(-4095, 4095, Integer(acc.Y*factor - acc.X*factor + acc.Rot*factor/2));
-                pwm.lb := Clamp(-4095, 4095, Integer(acc.Y*factor + acc.X*factor + acc.Rot*factor/2));
+                pwm.rf := Clamp(-4095, 4095, Integer(acc.Y*Factor + acc.X*Factor - acc.Rot*Factor/2));
+                pwm.rb := Clamp(-4095, 4095, Integer(acc.Y*Factor - acc.X*Factor - acc.Rot*Factor/2));
+                pwm.lf := Clamp(-4095, 4095, Integer(acc.Y*Factor - acc.X*Factor + acc.Rot*Factor/2));
+                pwm.lb := Clamp(-4095, 4095, Integer(acc.Y*Factor + acc.X*Factor + acc.Rot*Factor/2));
                 
                 dt := Clock - timer;
-                --Put_Line("Combine:");
-                --Put_Line("PERIOD= " & To_Duration(PERIOD)'Image);
-                --Put_Line("dt= " & To_Duration(dt)'Image);
-                --Put_Line("diff= " & To_Duration(PERIOD - dt)'Image);
             end if;
             delay until Clock + PERIOD;
         end loop;
-    end Move_Remote;
+    end Move_Radio;
     
-    -- Act
+    
     task body Motor_Control is
-        Threshold : constant := 100;
         speeds : Speeds2;
 
         timer : Time;
-        PERIOD : Time_Span := Milliseconds(80);
+        PERIOD : constant Time_Span := Milliseconds(80);
         dt : Time_Span;
     begin
         loop
@@ -285,14 +252,12 @@ package body TaskControl is
                 when others => Drive3(speeds);
             end case;
             dt := Clock - timer;
-            --Put_Line("Motor Control:");
-            --Put_Line("PERIOD= " & To_Duration(PERIOD)'Image);
-            --Put_Line("dt= " & To_Duration(dt)'Image);
-            --Put_Line("diff= " & To_Duration(PERIOD - dt)'Image);
             delay until Clock + PERIOD;
             end loop;
     end Motor_Control;
     
+    
+    -- Private functions
     function Clamp(min : Integer; max : Integer; value : Integer) return Integer is
     begin
         if value < min then
